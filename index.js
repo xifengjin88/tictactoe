98e22f3f2072c5dd06a7c986b71f536b8d83a125
Xifeng Jin <rjin@workmarket.com>
2019-03-05 18:57:40 -0500

    basic logic complete

--- index.js
+++ index.js
@@ -1,10 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Board from './src/components/board.component';
+import './index.css';
 
 class Game extends React.Component {
+  state = {
+    headerMessage: 'Welcome to our game',
+    currentPlayer: ''
+  };
+
+  handleTurnChange = turn => {
+    this.setState(() => ({
+      currentPlayer: turn
+    }));
+  };
+
+  handleDisplayMessage = ({ win = false, winner = null, draw = false }) => {
+    let headerMessage = 'Game is still going';
+    if (draw) {
+      headerMessage = 'Its a draw';
+    } else if (win) {
+      headerMessage = `${winner} is the winner`;
+    }
+    this.setState({
+      headerMessage
+    });
+  };
   render() {
-    return <Board />;
+    return (
+      <div>
+        <h1>{this.state.headerMessage}</h1>
+        <div className="btn-container">
+          <div
+            onClick={() => this.handleTurnChange('CPU')}
+            className="btn cpu-btn">
+            O
+          </div>
+          <div
+            onClick={() => this.handleTurnChange('PLAYER')}
+            className="btn player-btn">
+            X
+          </div>
+        </div>
+        <Board
+          handleDisplayMessage={this.handleDisplayMessage}
+          currentPlayer={this.state.currentPlayer}
+        />
+      </div>
+    );
   }
 }
 
